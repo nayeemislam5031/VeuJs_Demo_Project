8fbc01187d5c9df5bf209897ad96316c83d4a8d2
nayeem miah <nayeemislam5031@gmail.com>
2023-06-05 10:01:52 +0800

    second commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+
+<template>
+  
+  <h1>{{ title }} </h1>
+  <input type="text" ref="name">
+  <button @click="handleclick" >click me</button>
+  <Model/>
+  
+</template>
+
+<script >
+import Model from "./components/Model.vue"
+
+
+export default {
+  name:'App',
+  components:{Model},
+  data(){
+    return{
+      title:"This is the Vue :) "
+    }
+  },
+  methods: {
+    handleclick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add('active');
+      this.$refs.name.focus()
+    }
+  },
+
+}
+
+</script>
+
+<style scoped >
+
+h1{
+  text-align: center;
+  color: whitesmoke;
+  display: inline-block;
+  border-bottom: 1px solid #ddd;
+  padding-bottom:10px; 
+  font-size:large;
+}
+.active{
+  background-color: rgb(218, 224, 218);
+  /* color: white; */
+}
+
+
+</style>
---
+++ src/components/Model.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="backdrop">
+        <div class="model">
+            <h1> Model title</h1>
+            <p>model content</p>
+        </div>
+
+    </div>
+</template>
+
+
+<style  scoped>
+ .model{
+    width: 404px;
+    padding: 20px;
+    margin: 100px auto;
+    background: #fff;
+    border-radius: 10px;
+    color: black;
+    text-align: center;
+ }
+ .backdrop{
+    top: 0;
+    position: fixed;
+    background: #3f3e3e64;
+    width: 100%;
+    height: 100%;   
+ }
+ h1{
+    color: rgb(179, 22, 179);
+    border: none;
+    padding: 0;
+ }
+ .model p{
+    font-style: normal;
+ }
+</style>
